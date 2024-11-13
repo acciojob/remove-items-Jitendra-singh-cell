d0b4450fbd18672049ca4cfce18b77aa97b3ffac
Jitendra-singh-cell <jitendrasingh9x221@gmail.com>
2024-11-13 20:21:56 +0500

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+document.querySelector('input[type="button"]').addEventListener('click', function() {
+    var select = document.getElementById('colorSelect');
+    var selectedOption = select.options[select.selectedIndex];
+
+    // Remove the selected option if it is valid (i.e., something is selected)
+    if (selectedOption) {
+        select.removeChild(selectedOption);
+    }
+});
